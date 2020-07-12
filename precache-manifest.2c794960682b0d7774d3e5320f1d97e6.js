e54a8e27952086031c6c14380de81dfa6aeee952
unknown <laljeongho@gmail.com>
2020-07-12 22:20:34 +0900

    Updates

---
+++ precache-manifest.2c794960682b0d7774d3e5320f1d97e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d136a2a50e890c94e316d608755f2b8",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a7cb1310b2ff45410eae",
+    "url": "/movie_app_2020/static/css/main.3d9d8093.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7cb1310b2ff45410eae",
+    "url": "/movie_app_2020/static/js/main.c7f72520.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
